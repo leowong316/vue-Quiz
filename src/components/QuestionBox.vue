3659d6a3a24b208534f309f8a9ae107cef328f53
Leo <wong316xx@hotmail.com>
2019-05-30 13:23:19 +0800

    updated

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="question-box-container">
+    <b-jumbotron lead="Bootstrap 4 Components for Vue.js 2">
+      <template slot="lead">{{ currentQuestion.question }}</template>
+
+      <hr class="my-4">
+
+      <b-list-group>
+        <b-list-group-item
+          v-for="(answer,index) in answers"
+          :key="index"
+          @click="selectAnswer(index)"
+          :class="[selectedIndex === index ? 'selected':'']"
+        >{{ answer }}</b-list-group-item>
+      </b-list-group>
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button @click="next" variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function
+  },
+  data() {
+    return {
+      selectedIndex: null
+    };
+  },
+  computed: {
+    answers() {
+      //making a copy of the array instead of referecing the same array
+      let answers = [...this.currentQuestion.incorrect_answers];
+      answers.push(this.currentQuestion.correct_answer);
+      return answers;
+    }
+  },
+  methods: {
+    selectAnswer(index) {
+      this.selectedIndex = index;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.list-group {
+  margin-bottom: 15px;
+}
+.list-group-item:hover {
+  background-color: #eee;
+  cursor: pointer;
+}
+.btn {
+  margin: 0 5px;
+}
+.selected {
+  background-color: lightblue;
+}
+.correct {
+  background-color: lightgreen;
+}
+.incorrect {
+  background-color: red;
+}
+</style>
+
+
